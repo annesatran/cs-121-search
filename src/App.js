bbca8d679c23c67faf1ec85e563ef0fb207ff1b2
Annesa Tran <annesat2020@gmail.com>
2023-11-30 01:17:08 -0800

    Frontend base finished

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SearchPage from "./components/SearchPage.jsx"
+
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <SearchPage />
+    </div>
+  );
+}
+export default App;
